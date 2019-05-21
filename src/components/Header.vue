2a34361f8308bd894ac7d61b56d51de8aaed2ded
rjwu95 <yugun000@gmail.com>
2019-05-21 23:27:35 +0900

    git init

---
+++ src/components/Header.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="hello">
+    <button @click="goToHome">home</button>
+    <button @click="goToTest">test</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data: () => ({
+    msg: 'Welcome to Your Vue.js App'
+  }),
+  methods: {
+    moveRoute: function (path) {
+      this.$router.push(`/${path}`)
+    },
+    goToHome: this.moveRoute(''),
+    goToTest: this.moveRoute('test')
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/Test.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="test">
+    <h1>{{ msg }}</h1>
+    <h2>test one two one two</h2>
+    <button v-on:click="goToHome">go to home</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Test',
+  data () {
+    return {
+      msg: 'this is test page'
+    }
+  },
+  methods: {
+    goToHome: function () {
+      fetch('https://koreanjson.com/users').then((res) => {
+        console.log(this)
+      })
+      this.$router.push('/')
+    }
+  }
+}
+</script>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1,
+h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
